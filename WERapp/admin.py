c487f781717ae3c5d59ea049aa75cfa9910b3ef0
Callum Watson <2310586w@student.gla.ac.uk>
2019-02-28 14:16:01 +0000

    Review Database

--- WERapp/admin.py
+++ WERapp/admin.py
@@ -1,3 +1,3 @@
 from django.contrib import admin
-
-# Register your models here.
+from WERapp.models import Review
+admin.site.register(Review)
--- WERapp/models.py
+++ WERapp/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+
+#class UserProfile(models.Model):
+#    user = models.OneToOneField(User)
+
+class Review(models.Model):
+   
+    reviewID = models.IntegerField(unique=True)
+    #user
+    date = models.DateField(auto_now=True)
+    comment = models.CharField(max_length=200, default="Default")  
+    price = models.IntegerField(default=0)
+    quality = models.IntegerField(default=0)
+    atmostphere = models.IntegerField(default=0)
+    avgRating = models.IntegerField(default=0)
+    
+    def __str__(self):
+        return str(self.reviewID)
